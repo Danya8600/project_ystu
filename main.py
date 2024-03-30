c7ede1bd3fc4be2695de8f68b59d7fd61623ab8e
Danya <daniiltihomirov003@gmail.com>
2024-03-30 23:10:31 +0300

    подключил кнопку к методу, которыц меняет текст на ней. коммит пробный, для дальнейше

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-# проверка на работоспособность push
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('reg.ui', self)
+        self.enter_btn.clicked.connect(self.enter)
+
+    def enter(self):
+        self.enter_btn.setText("ok")
 
 
 if __name__ == '__main__':
