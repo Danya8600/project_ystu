8e75f493e37293892334fb9d8049956cce32ae69
DjokSe <misha.stepanov.2018@mail.ru>
2024-05-20 13:37:47 +0300

    Реагирует на кнопку, код проходит успешно, но не появляется новая запись

--- Registr.py
+++ Registr.py
@@ -19,7 +19,7 @@ class WindowRegistr(QWidget):
         password = self.lineEdit_5.text()
         pas2 = self.lineEdit_6.text()
         tel = self.lineEdit_7.text()
-        dol = self.comboBox.text()
+        dol = self.comboBox.currentText()
         if len(fam) == 0:
             return
         if len(im) == 0:
@@ -30,9 +30,12 @@ class WindowRegistr(QWidget):
             return
         if len(tel) == 0:
             return
-        query = f"SELECT COUNT(*) FROM {log_pswd}"
-        cursor.execute(query)
-        result = cursor.fetchone()
+        table_name='log_pswd'
+        query = f"SELECT COUNT(*) FROM {table_name}"
+        con = sqlite3.connect("All_data.db")  # подключил бд с аккаунтами
+        cur = con.cursor()
+        cur.execute(query)
+        result = cur.fetchone()
         row_count = result[0]
         id_user = row_count+1
         if dol == "Администратор":
@@ -42,8 +45,11 @@ class WindowRegistr(QWidget):
                 id_level = 2
             else:
                 id_level = 3
-        cursor.execute(f'SELECT login FROM log_pswd WHERE login="{login}"')
-        if cursor.fetchone() is None:
-            cursor.execute(f'INSERT INTO log_pswd VALUES ("{row_count}", "{login}", "{password}", "{id_level}")')
-            cursor.execute(f'INSERT INTO lich_dan VALUES ("{fam}", "{im}", "{otch}", "{tel}")')
+        cur.execute(f'SELECT login FROM log_pswd WHERE login="{login}"')
+        if cur.fetchone() is None:
+            print('6')
+            cur.execute(f'INSERT INTO log_pswd VALUES ("{id_user}", "{login}", "{password}", "{id_level}")')
+            print('7')
+            cur.execute(f'INSERT INTO lich_dan VALUES ("{id_user}", "{fam}", "{im}", "{otch}", "{tel}")')
+
         print(id_user)
