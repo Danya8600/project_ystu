2a828fe0ad9d4e27d3ade1a1bdcf7b67375b1f34
Daniil Tihomirov <daniiltihomirov003@gmail.com>
2024-05-24 12:18:51 +0300

    теперь приветствуется именем и фамилией, а не логином пользователя

--- admin.py
+++ admin.py
@@ -11,13 +11,23 @@ class WindowAdmin(QWidget):
     def __init__(self, email):
         super().__init__()
         uic.loadUi('window_admin.ui', self)
+
         self.email = email
+        con = sqlite3.connect("All_data.db")  # подключил бд с аккаунтами
+        cur = con.cursor()
+        cur.execute(f'SELECT id_user FROM log_pswd WHERE login="{self.email}"')
+        id_result = cur.fetchone()[0]
+        cur.execute(f'SELECT Фамилия FROM lich_dan WHERE id_user="{id_result}"')
+        self.surname = cur.fetchone()[0]
+        cur.execute(f'SELECT Имя FROM lich_dan WHERE id_user="{id_result}"')
+        self.name = cur.fetchone()[0]
+
         self.setWindowTitle("Администратор")
         self.setWindowIcon(QIcon('лого.png'))
         self.exitbtn.clicked.connect(self.exit)
         self.registrbtn.clicked.connect(self.regi)
         self.Oknozadbtn.clicked.connect(self.sozdzad)
-        self.name_label.setText(f'Привет, {self.email}')
+        self.name_label.setText(f'Привет, {self.surname} {self.name}')
 
     def exit(self):
         self.close()
