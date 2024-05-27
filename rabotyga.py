8de686dfc4c8336f50e968559f5bbb3da2acc60a
Daniil Tihomirov <daniiltihomirov003@gmail.com>
2024-05-27 17:59:32 +0300

    Теперь и у менеджеров с рабочими показывается зп

--- rabotyga.py
+++ rabotyga.py
@@ -12,4 +12,19 @@ class WindowRabotyga(QWidget):
         self.email = email
         self.setWindowTitle("Рабочий")
         self.setWindowIcon(QIcon('лого.png'))
-        self.name_label.setText(f'Привет, {self.email}')
+        self.email = email
+        con = sqlite3.connect("All_data.db")  # подключил бд с аккаунтами
+        cur = con.cursor()
+
+        cur.execute(f'SELECT id_user FROM log_pswd WHERE login="{self.email}"')
+        self.id_result = cur.fetchone()[0]
+        cur.execute(f'SELECT Фамилия FROM lich_dan WHERE id_user="{self.id_result}"')
+        self.surname = cur.fetchone()[0]
+        cur.execute(f'SELECT Имя FROM lich_dan WHERE id_user="{self.id_result}"')
+        self.name = cur.fetchone()[0]
+
+        cur.execute(f'SELECT Зарплата FROM Зарплата WHERE id_user="{self.id_result}"')
+        self.zp = cur.fetchone()[0]
+        self.label_zp.setText(self.zp)
+
+        self.name_label.setText(f'Привет, {self.surname} {self.name}')
