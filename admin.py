409d356e8b555009d56a7fcf263a2de5c77ed663
Daniil Tihomirov <daniiltihomirov003@gmail.com>
2024-05-27 17:44:19 +0300

    Добавил показ заработной платы пользователя на странице админа

--- admin.py
+++ admin.py
@@ -15,12 +15,17 @@ class WindowAdmin(QWidget):
         self.email = email
         con = sqlite3.connect("All_data.db")  # подключил бд с аккаунтами
         cur = con.cursor()
+
         cur.execute(f'SELECT id_user FROM log_pswd WHERE login="{self.email}"')
         self.id_result = cur.fetchone()[0]
         cur.execute(f'SELECT Фамилия FROM lich_dan WHERE id_user="{self.id_result}"')
         self.surname = cur.fetchone()[0]
         cur.execute(f'SELECT Имя FROM lich_dan WHERE id_user="{self.id_result}"')
         self.name = cur.fetchone()[0]
+
+        cur.execute(f'SELECT Зарплата FROM Зарплата WHERE id_user="{self.id_result}"')
+        self.zp = cur.fetchone()[0]
+        self.label_zp.setText(self.zp)
 
         self.setWindowTitle("Администратор")
         self.setWindowIcon(QIcon('лого.png'))
